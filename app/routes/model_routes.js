e49990b8bd43a2c0538f725114f5a965852846a2
Sam Rudd <samdrudd@gmail.com>
2025-04-10 22:56:51 -0700

    Update template for the year 2025

--- app/routes/model_routes.js
+++ app/routes/model_routes.js
@@ -3,61 +3,70 @@ const ModelModel = require('../models/Model');
 module.exports = function(app, db) {
 	const Model = db.model('Model', ModelModel.modelSchema, 'models');
 
+    // Create a Model
 	app.post('/models', (req, res) => {
 		var model = new Model(req.body); 
-		model.save((err) => {
-			if (err) {
-				console.log(err);
-				res.status(500).send( {"error" : err} );
-			}
-			else
-				res.send(model);
-		});
+		model.save().then((savedDoc) => {
+			res.status(201).json(savedDoc);
+		})
+        .catch(error => {
+            console.log("Error saving Model: " + error);
+            res.status(500).json({message: 'Error saving Model', error: error.message});
+        });
 	});
 
 
+    // Find a Model by ID
 	app.get('/models/:id', (req, res) => {
-		Model.findById(req.params.id, (err, results) => {
-			if (err) {
-				console.log(err);
-				res.status(500).send({"error" : err});
-			}
-			else
-				res.send(results);
-		});
+		Model.findById(req.params.id).then((foundDoc) => {
+            if (foundDoc)
+			    res.status(200).send(foundDoc);
+            else
+                res.status(404).json({message: "Model not found"});
+		})
+        .catch(error => {
+            console.log("Error searching for Model: " + error);
+            res.status(500).json({message: "Error searching for Model", error: error.message});
+        });
 	});
 
+    // Find all Models
 	app.get('/models', (req, res) => {
-		Model.find({}, (err, results) => {
-			if (err) {
-				console.log(err);
-				res.status(500).send({"error" : err});
-			}
-			else
-				res.send(results);
-		});
+		Model.find({}).then((results) => {
+			res.send(results);
+		})
+        .catch(error => {
+            console.log("Error retrieving all Models: " + error);
+            res.status(500).json({message: "Error retrieving all Models", error: error.message});
+        });
 	
 	});
 
+    // Update a Model
 	app.put('/models/:id', (req, res) => {
-		Model.update( {'_id' : req.params.id}, req.body, (err, result) => {
-			if (err) {
-				console.log(err);
-				res.status(500).send( {"error" : err} );
-			}
-			else
-				res.send(result);
-		});
+        Model.findByIdAndUpdate(req.params.id, req.body, {new: true}).then((savedDoc) => {
+            if (savedDoc)
+			    res.send(savedDoc);
+            else
+                res.status(404).json({message: "Model not found"});
+		})
+        .catch(error => {
+            console.log("Error updating Model: " + error);
+            res.status(500).json({message: "Error updating Model", error: error.message});
+        });
 	});
 
+    // Delete a Model
 	app.delete('/models/:id', (req, res) => {
-		Model.remove( {'_id' : req.params.id}, (err, result) => {
-			if (err) {
-				console.log(err);
-				res.status(500).send( {"error" : err} );
-			}
-			else
-				res.send(result);
-		});
+		Model.findByIdAndDelete(req.params.id).then((deletedDoc) => {
+            if (deletedDoc)
+			    res.send(deletedDoc);
+            else
+                res.status(404).json({message: "Model not found"});
+		})
+        .catch(error => {
+            console.log("Error deleting Model: " + error);
+            res.status(500).json({message: "Error deleting Model", error: error.message});
+        });
 	});
 };
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express		= require('express');
 const cors  		= require('cors');
 const mongoose		= require('mongoose');
-const bodyParser	= require('body-parser');
-const db		= require('./config/db');
+const bodyParser	= require('body-parser').urlencoded({ extended: true });
+const db		    = require('./config/db');
 
 const port = process.argv[3] || 8000;
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser);
 
 var database = mongoose.createConnection(db.url);
 
